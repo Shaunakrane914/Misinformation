5f252287faaa7fe83b96a7bfc295094d45df26e1
Shaunakrane914 <shaunakrane914@gmail.com>
2026-09-16 23:31:00 +0530

    fix: bind server to 0.0.0.0 for universal connectivity

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     import uvicorn
     import os
     port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("backend.main:app", host="127.0.0.1", port=port, reload=False)
+    uvicorn.run("backend.main:app", host="0.0.0.0", port=port, reload=False, access_log=True)
